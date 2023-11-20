fb81fcfd5ddd05bb13f011e4ea307e4709d9010b
Bouchra Akl <bushraakl1234@gmail.com>
2023-11-20 20:56:35 -0300

    feat : filters iniciated

--- src/app/components/relatorio/relatorio.component.ts
+++ src/app/components/relatorio/relatorio.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { Consulta } from 'src/app/models/consulta/consulta';
+import { ConsultaService } from 'src/app/services/consulta/consulta.service';
+import { Header } from '../table/header';
+import { Usuario } from 'src/app/models/usuario/usuario';
+import { environment } from 'src/environments/environment';
+import { Animal } from 'src/app/models/animal/animal';
 
 @Component({
   selector: 'app-relatorio',
@@ -6,5 +14,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./relatorio.component.scss']
 })
 export class RelatorioComponent {
+  modalRef!: NgbModalRef;
+  modalService = inject(NgbModal)
+  @Output() confirmarEvent = new EventEmitter<Consulta>();
+  @Input() consultaSelecionada!: Consulta
+  keys = Object.keys;
+  isErro = false;
+  mensagem!: string;
+  disabled = false;
+  consulta = new Consulta();
+  service = inject(ConsultaService);
+  router = inject(Router)
+  showButton = false;
+  radio1: boolean = false;
+  radio2: boolean = false;
 
+
+  abrirModal(template: any) {
+    this.modalRef = this.modalService.open(template, {
+      size: 'lg',
+      centered: true,
+    });
+  }
+
+  onRadioChange() {
+    this.showButton = true;
+  }
+
+  onRadioChangeOther() {
+    this.showButton = false;
+  }
+
+  definirAnimal(animal: Animal) {
+    this.consulta.animal = animal;
+    this.modalRef.close();
+  }
+
+  callAnimalHeaders() {
+    let tableHeaders: Header[] = [];
+    tableHeaders.push(new Header('Nome', 'nome'));
+    tableHeaders.push(new Header('Tutor', 'tutorId.nome'));
+    tableHeaders.push(new Header('Raça', 'raca'));
+    tableHeaders.push(new Header('Espécie', 'especie'));
+    tableHeaders.push(new Header('Sexo', 'sexo'));
+    return tableHeaders;
+  }
+
+  getAnimalUrl(){
+    return `${environment.apiUrl}/animal`
+  }
 }
